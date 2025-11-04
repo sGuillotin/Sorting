96ffff10d9d92afd5a279bd467f4748833ad18f8
Sébastien Guillotin <sebgusa@gmail.com>
2025-11-04 09:14:09 -0500

    Start project and c# sort

--- Program.cs
+++ Program.cs
@@ -5,6 +5,40 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            int[] numbers1 = { -11, 12, -42, 0, 1, 90, 68, 6, -9 };
+            int[] numbers2 = { -11, 12, -42, 0, 1, 90, 68, 6, -9 };
+
+            Console.Write("Before using Bubble Sort: ");
+            foreach(int i in numbers1) {
+                Console.Write(i + " ");
+            }
+
+            //studentBubbleSort(numbers1);
+
+            Console.Write("\nAfter using Bubble Sort:");
+            foreach (int i in numbers1)
+            {
+                Console.Write(i + " ");
+            }
+
+
+
+
+
+            Console.Write($"\nBefore using C# Sort: ");
+            foreach (int i in numbers2)
+            {
+                Console.Write(i + " ");
+            }
+
+            Array.Sort(numbers2);
+
+            Console.Write("\nAfter using C# Sort: ");
+            foreach (int i in numbers2)
+            {
+                Console.Write(i + " ");
+            }
         }
     }
 }
