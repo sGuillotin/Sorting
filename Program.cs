9ef6fcf013a063bb80625c419d75f73229aa7a03
Sébastien Guillotin <sebgusa@gmail.com>
2025-11-06 20:48:43 -0500

    Attempt to write bubble sort
    numerous syntax errors. I remember nothing from my Jr year Java class.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Sorting
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Sorting
 {
     internal class Program
     {
@@ -9,19 +11,20 @@
             int[] numbers1 = { -11, 12, -42, 0, 1, 90, 68, 6, -9 };
             int[] numbers2 = { -11, 12, -42, 0, 1, 90, 68, 6, -9 };
 
+
+
             Console.Write("Before using Bubble Sort: ");
             foreach(int i in numbers1) {
                 Console.Write(i + " ");
             }
 
-            //studentBubbleSort(numbers1);
+            studentBubbleSort(numbers1);
 
             Console.Write("\nAfter using Bubble Sort:");
             foreach (int i in numbers1)
             {
                 Console.Write(i + " ");
             }
-
 
 
 
@@ -38,6 +41,24 @@
             foreach (int i in numbers2)
             {
                 Console.Write(i + " ");
+            }
+        }
+
+        //student made bubble sort function
+        public static void studentBubbleSort(int[] intArr)
+        {
+            for (int i = 0; i < intArr.Length - 1; i++)
+            {
+                for (int j = 0; j < intArr.Length - i - 1; j++)
+                {
+                    if (intArr[j] > intArr[j + 1])
+                    {
+                        // swap temp and arr[i]
+                        int temp = intArr[j];
+                        intArr[j] = intArr[j + 1];
+                        intArr[j + 1] = temp;
+                    }
+                }
             }
         }
     }
